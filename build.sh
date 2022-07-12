95a035c65a081f918a7f73399de5db22de035ec1
Jiří Stránský <jistr@jistr.com>
2022-07-12 17:44:58 +0200

    Add more tools like virt-manager, virt-viewer, libguestfs-tools

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 set -euxo pipefail
 
 dnf -y update --refresh
-dnf -y install nss-altfiles libvirt qemu-kvm
+dnf -y install \
+    nss-altfiles\
+    libguestfs-tools \
+    libvirt \
+    libvirt-daemon-kvm \
+    qemu-kvm \
+    virt-manager \
+    virt-viewer
 dnf clean all
 
 mv /etc/libvirt /etc/libvirt-default
